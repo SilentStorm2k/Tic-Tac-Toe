74396ab350c93033289e49aee256f0c5e5f34483
SilentStorm2k <47686604+SilentStorm2k@users.noreply.github.com>
2024-02-02 23:14:49 +0530

    feat, fix: Added game which simulates tictactoe, fixed gameOver check to account for undefined row/col values

--- js/index.js
+++ js/index.js
@@ -13,8 +13,13 @@ const GameBoard = (function () {
 
     // Update state when value of ith cell DNE
     const updateGameState = (i, cell) => {
-        if (gameState[i].getValue() == undefined && i < gameState.length)
+        if (typeof gameState[i] == 'undefined' || typeof cell == 'undefined' || typeof i != 'number')
+            return false;
+        if (typeof gameState[i].getValue() == 'undefined' && i < gameState.length) {
             gameState.splice(i, 1, cell);
+            return true;
+        }
+        return false;   
     };
     const isGameOver = () => {
         const size = Math.sqrt(gameState.length);
@@ -33,6 +38,8 @@ const GameBoard = (function () {
             for (let col = 0; col < size; col++) {
                 r = flip ? col : row;
                 c = flip ? row : col;
+                if (typeof gameState[r*size + c].getValue() == 'undefined')
+                    continue;
                 if (col == 0) 
                     curElement = gameState[r*size + c].getValue();
                 if (curElement == gameState[r*size + c].getValue())
@@ -49,22 +56,24 @@ const GameBoard = (function () {
     const checkDiagonals = (size) => {
         const mainDiag = gameState[0].getValue(), antiDiag = gameState[size-1].getValue();
         const player1 = gameState[0].getOwner(), player2 = gameState[size-1].getOwner();
-        let allSameInMainDiag = true, allSameInAntiDiag = true;
+        let countInMainDiag = 0, countInAntiDiag = 0;
         for (let row = 0; row < size; row++) {
             for (let col = 0; col < size; col++) {
-                if (row == col && allSameInMainDiag) {
-                    allSameInMainDiag = mainDiag == gameState[row*size + col].getValue() ? true : false;
+                if (row == col) {
+                    countInMainDiag = mainDiag == gameState[row*size + col].getValue() ? countInMainDiag + 1 : 0;
                 }
-                if (row+col+1 == size && allSameInAntiDiag) {
-                    allSameInAntiDiag = antiDiag == gameState[row*size + col].getValue() ? true : false;
+                if (row+col+1 == size) {
+                    countInAntiDiag = antiDiag == gameState[row*size + col].getValue() ? countInAntiDiag + 1 : 0;
                 }
             }
         }
-        if (allSameInMainDiag)
+        countInMainDiag = typeof mainDiag == 'undefined' ? 0 : countInMainDiag;
+        countInAntiDiag = typeof antiDiag == 'undefined' ? 0 : countInAntiDiag;
+        if (countInMainDiag == size)
             winner = player1;
-        if (allSameInAntiDiag)
+        if (countInAntiDiag == size)
             winner = player2;
-        return allSameInMainDiag || allSameInAntiDiag;
+        return countInMainDiag == size || countInAntiDiag == size;
     };
 
     const showGameState = () => {
@@ -106,42 +115,71 @@ function Player (playerName, playerMarker) {
     return {getMarker, getPlayer, greetPlayer};
 };
 
-function Game () {
-    const size = GameBoard.getValue().length;
-    const getUserInput = prompt("Enter which square", 0);
+const Game = (function () {
     const startGame = () => {
         GameBoard.createGameBoard();
-
+        const size = Math.sqrt(GameBoard.getGameState().length);
+        const player = Player(prompt("Enter your name: ", "John Doe"), "X");
+        const computer = Player("comp", "O");
+        const userCell = Cell(), compCell = Cell();
+        const validChoices = [];
+        for (let i = 0; i < GameBoard.getGameState().length; i++) 
+            validChoices.push(i);
+        let cellLocation = 0, row = 0; col = 0;
+        userCell.assignOwner(player);
+        compCell.assignOwner(computer);
+        GameBoard.showGameState();
+        while (!GameBoard.isGameOver()) {
+            row = prompt("Enter row", 0);
+            col = prompt("Enter col", 0);
+            cellLocation = parseInt(row)*size + parseInt(col);
+            while (!GameBoard.updateGameState(cellLocation, userCell)) {
+                row = prompt("Prev invalid, Enter row", 0);
+                col = prompt("Prev invalid, Enter col", 0);
+                cellLocation = parseInt(row)*size + parseInt(col);
+            }
+            validChoices.splice(validChoices.indexOf(cellLocation), 1);
+            console.log("USer choide = ", cellLocation);
+            cellLocation = Math.floor(Math.random()*validChoices.length);
+            GameBoard.updateGameState(validChoices[cellLocation], compCell);
+            console.log("Comp choide = ", validChoices[cellLocation]);
+            validChoices.splice(cellLocation, 1);
+            GameBoard.showGameState();
+        }
+        console.log(`Winner winner tofu dinner : ${GameBoard.getWinner()} has won the game`)
     }
+    return {startGame};
+})();
 
-}
+Game.startGame();
 
-const p1 = Player("shivs", "x");
-const p2 = Player("Comp", 'O');
-// p1.greetPlayer();
-// console.log(p1.getMarker());
+// const p1 = Player("shivs", "x");
+// const p2 = Player("Comp", 'O');
+// // p1.greetPlayer();
+// // console.log(p1.getMarker());
 
-const c1 = Cell();
-c1.assignOwner(p1);
-console.log(c1.getValue());
+// const c1 = Cell();
+// c1.assignOwner(p1);
+// console.log(c1.getValue());
 
-const c2 = Cell();
-c2.assignOwner(p2);
+// const c2 = Cell();
+// c2.assignOwner(p2);
 
 
-GameBoard.createGameBoard();
-console.log(GameBoard.getGameState());
+// GameBoard.createGameBoard();
+// // console.log(GameBoard.getGameState());
 
-GameBoard.updateGameState(0, c1);
-GameBoard.updateGameState(1, c2);
-GameBoard.updateGameState(2, c1);
-GameBoard.updateGameState(3, c1);
-GameBoard.updateGameState(4, c1);
-GameBoard.updateGameState(5, c2);
-GameBoard.updateGameState(6, c2);
-GameBoard.updateGameState(7, c2);
-GameBoard.updateGameState(8, c1);
-console.log(GameBoard.getGameState());
-GameBoard.showGameState();
-console.log(GameBoard.isGameOver());
-console.log("winner = ", GameBoard.getWinner())
+// GameBoard.updateGameState(0, c1);
+// GameBoard.updateGameState(1, c2);
+// GameBoard.updateGameState(2, c1);
+// GameBoard.updateGameState(3, c1);
+// GameBoard.updateGameState(4, c1);
+// GameBoard.updateGameState(5, c2);
+// GameBoard.updateGameState(6, c2);
+// GameBoard.updateGameState(7, c2);
+// console.log(GameBoard.updateGameState(8, c1));
+// console.log(GameBoard.updateGameState(8, c2));
+// // console.log(GameBoard.getGameState());
+// GameBoard.showGameState();
+// console.log(GameBoard.isGameOver());
+// console.log("winner = ", GameBoard.getWinner())
